01ca10e07a913f83786978fd746aef5da1cc8b63
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-15 23:54:35 -0500

    75. Tarea - Impresión de consola y archivo

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log('Hola Mundo');
+import fs from 'fs';
+
+let outputMessage = '';
+const base = 5;
+const headerMessage = `
+======================
+   Tabla del ${base}
+======================\n
+`;
+
+for (let i = 1; i < 10; i++) {
+	outputMessage += `${base} x ${i} = ${base * i} \n`;
+}
+
+outputMessage = headerMessage + outputMessage;
+
+console.log(outputMessage);
+
+const outputPath = `outputs`;
+
+fs.mkdirSync(outputPath, { recursive: true });
+
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
+
+console.log('File created!');
