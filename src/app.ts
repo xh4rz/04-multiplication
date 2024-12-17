4ce780977e547576ff7ef900a29fb327ba66d03b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-17 15:38:25 -0500

    82. Refactorizar - Organizar lógicamente el código

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import { yarg } from './config/plugins/args.plugin';
+import { ServerApp } from './presentation/server-app';
 
 (async () => {
 	await main();
 })();
 
 async function main() {
-	console.log(yarg);
+	const { b: base, l: limit, s: showTable } = yarg;
+
+	ServerApp.run({
+		base,
+		limit,
+		showTable
+	});
 }
