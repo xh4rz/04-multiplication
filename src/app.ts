6b826ea31d742218b169f6194e70d57ac48bac60
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-12-17 16:52:54 -0500

    87. Solución de la tarea

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,19 @@ import { ServerApp } from './presentation/server-app';
 })();
 
 async function main() {
-	const { b: base, l: limit, s: showTable } = yarg;
+	const {
+		b: base,
+		l: limit,
+		s: showTable,
+		n: fileName,
+		d: fileDestination
+	} = yarg;
 
 	ServerApp.run({
 		base,
 		limit,
-		showTable
+		showTable,
+		fileName,
+		fileDestination
 	});
 }
--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -5,17 +5,26 @@ interface RunOptions {
 	base: number;
 	limit: number;
 	showTable: boolean;
+	fileName: string;
+	fileDestination: string;
 }
 
 export class ServerApp {
-	static run({ base, limit, showTable }: RunOptions) {
+	static run({
+		base,
+		limit,
+		showTable,
+		fileName,
+		fileDestination
+	}: RunOptions) {
 		console.log('Server running...');
 
 		const table = new CreateTable().execute({ base, limit });
 
 		const wasCreated = new SaveFile().execute({
 			fileContent: table,
-			fileDestination: `outputs/table-${base}`
+			fileDestination,
+			fileName
 		});
 
 		if (showTable) console.log(table);
